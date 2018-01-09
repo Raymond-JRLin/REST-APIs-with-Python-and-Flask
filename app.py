7a3f42f6b286a35a8567de290ca0391579d33580
Junrui Line <raymond.jr.lin@gmail.com>
2018-01-09 16:05:13 -0500

    created 4 functions and definition of stores

--- app.py
+++ app.py
@@ -3,12 +3,58 @@
 # tell Python we want to use flask
 from flask import Flask
 
-app = Flask(__name__) # create a Flask object with unique name 
+app = Flask(__name__) # create a Flask object with unique name
 
-# create a route for homepage
-@app.route('/') # end-point with /
-def home():
-	# return a response to browsers
-	return "Hello, world!"
+# create a list
+stores = [
+	{
+		'name': 'My Wonderful Store'
+		'item': [
+			{
+				'name': 'My Item'
+				'price': 15.99
+			}
+		]
+	}
+]
+
+###
+# # create a route for homepage
+# @app.route('/') # end-point with /
+# def home():
+# 	# return a response to browsers
+# 	return "Hello, world!"
+###
+
+# from server perspective:
+# POST: used to recerive data
+# GET: used to send data back only
+
+# POST /store data: {name: }
+@app.route('/store', methods = ['POST']) # default rout is a GET
+def create_store():
+	pass
+
+# GET /store/<string: name>
+@app.route('/store/<string: name>') # http://127.0.0.1:5000/store/some_name
+def get_store(name):
+	pass
+
+# GET /store
+@app.route('/store')
+def get_stores():
+	pass
+
+# POST /store/<string: name>/item {name: , price }
+@app.route('/store/<string: name>/item', methods = ['POST'])
+def create_item(name):
+	pass
+
+
+# GET /store/<string: name>/item
+@app.route('/store/<string: name>/item')
+def get_item_in_store(name):
+	pass
+
 
 app.run(port = 5000)
