a986361079b3bbbb91e6b8e2ff63a32b830790c4
Junrui Line <raymond.jr.lin@gmail.com>
2018-01-10 15:56:41 -0500

    added Flask_JWT package

--- code/app.py
+++ code/app.py
@@ -1,13 +1,21 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
 # resource is a thing can be created and changed by API
+from flask_jwt import JWT, jwt_required
+
+from security import authenticate, identity
 
 app = Flask(__name__)
+app.secret_key = 'Raymond' # use a secret key and JWT, which stands for JSON Web Token, to encrypt message -> add security.py
 api = Api(app)
+
+jwt = JWT(app, authenticate, identity) # JWT creates a new endpoint of /auth, including a user name and password, then JWT sent them to authenticate function to compare, and returns a JW token. JWT itself can do nothing, but send to identity function
 
 items = []
 
 class Item(Resource):
+    @jwt_required()
+    # we have to authenticate before we can call the get method
     def get(self, name):
         ###
         # for item in items:
@@ -18,15 +26,15 @@ class Item(Resource):
         item = next(filter(lambda x : x['name'] == name, items), None) # lambda returns not a item or list but lambda object, so we can return a list of items whose names match the searching name, e.g.: list(filter(lambda x : x['name'] == name, items)). But we know there's exact only one item having the given name, so we use next to return the first item found by this filter funtion. Similarly, we can use one more next to get 2nd mathced item and then 3rd and so on. But it may raise a problem if there's no such matched item left, then next would break our program. So we set a default value as None, which means if there's no matched item, then return none
         # return {'item': None}, 404 # return a JSON
         # 200 is the most popular http status code, but 404 NOT FOUND is the correct one for not found
-        # return {'item': None}, 200 if item is not None else 404
+        # return {'item': item}, 200 if item is not None else 404
         # shorten:
-        return {'item': None}, 200 if item else 404
+        return {'item': item}, 200 if item else 404
 
     def post(self, name):
         # improvement: to make sure there's only unique names for items
         if next(filter(lambda x : x['name'] == name, items), None) is not None:
             # there's an item having the name
-            return {'message': 'An item with name '{}' already exists.'.format(name)}, 400 # 400 is bad request, because it's not our fault but client's since they should have verified it's not an existing name, but we can tell them it's an invalid input because it's already in this app
+            return {'message': "An item with name '{}' already exists.".format(name)}, 400 # 400 is bad request, because it's not our fault but client's since they should have verified it's not an existing name, but we can tell them it's an invalid input because it's already in this app
 
         # pass JSON payload, but there would be some problem if it's not a JSON or there's no header or body. There's 2 ways to handle:
         # method 1:
