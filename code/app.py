52cd34d59fbde39d287d94bf3625c79abe25f251
Junrui Line <raymond.jr.lin@gmail.com>
2018-01-10 16:13:14 -0500

    implemented PUT method

--- code/app.py
+++ code/app.py
@@ -58,6 +58,19 @@ class Item(Resource):
         items = list(filter(lambda x : x['name'] != name, items))
         return {'message': 'Item deleted'}
 
+    def put(self, name):
+        data = request.get_json()
+        item = next(filter(lambda x : x['name'] == name, items), None)
+        if item is None:
+            # create a new item
+            item = {'name': name, 'price': data['price']}
+            item.append(item)
+        else:
+            # it exists already -> update it
+            # item is a dictionary and dictionary has update method
+            item.update(data)
+        return item
+
 class ItemList(Resource):
     def get(self):
         return {'items': items}
