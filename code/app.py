46ca8901b39ace2787d0eb534c200cc51a885cdb
Junrui Line <raymond.jr.lin@gmail.com>
2018-01-10 14:44:29 -0500

    added get ItemList endpoint and did some improvement slightly on GET and POST

--- code/app.py
+++ code/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 # resource is a thing can be created and changed by API
 
@@ -16,7 +16,14 @@ class Item(Resource):
         # 200 is the most popular http status code, but 404 NOT FOUND is the correct one for not found
 
     def post(self, name):
-        item = {'name': name, 'price': 12.00}
+        # pass JSON payload, but there would be some problem if it's not a JSON or there's no header or body. There's 2 ways to handle:
+        # method 1:
+        # data = request.get_json(force = True) # you don't need the content-type header just look the content and format it even if the content-type is not set to be application/JSON
+        # but it's dangerouse since if you don't use that it will look into header and get an error, if you use that, it will not look in header and get even incorrect content
+        # method 2:
+        # data = request.get_json(silent = True) # it doesn't give an error, it just basically returns none
+        data = request.get_json()
+        item = {'name': name, 'price': data['price']}
         items.append(item) # add it into items list
         return item, 201 # tell client it succeeds
         # 200 OK is for when the server just kind of returns some data, and everything's ok and I've given what you wanted
@@ -25,6 +32,12 @@ class Item(Resource):
 
         # so it's important to use correct status code for client to check whether things went wrong or not
 
-api.add_resource(Item, '/item/<string:name>') # http://127.0.0.1:5000/student/Rolf
+class ItemList(Resource):
+    def get(self):
+        return {'items': items}
 
-app.run(port = 5000)
+
+api.add_resource(Item, '/item/<string:name>') # http://127.0.0.1:5000/student/Rolf
+api.add_resource(ItemList, '/items')
+
+app.run(port = 5000, debug = True) # Flask is nice to show the error message
