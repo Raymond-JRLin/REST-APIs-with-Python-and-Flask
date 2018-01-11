a5c49f356a432db03c242cfd939dd2e2edc95674
Junrui Line <raymond.jr.lin@gmail.com>
2018-01-10 19:42:58 -0500

    retrieve data from database

--- store-resource-in-sql-db/test.py
+++ store-resource-in-sql-db/test.py
@@ -1,22 +1,32 @@
 import sqlite3 # already in Python, able to connect and run queries
 
+# build a connection between sqlite db and Python
 connection = sqlite3.connect('data.db') # connection string - URI: Unified Resource Identifier
 
 cursor = connection.cursor() # select and start things, responsible for quering and store the results
 
+# create a table
 create_table = "CREATE TABLE users (id int, username text, password text)" # <table_name> (<columns>)
 cursor.execute(create_table)
 
+# insert a tuple
 user = (1, 'Raymond', 'ray') # Python tuple
 insert_query = "INSERT INTO users VALUES (?, ? , ?)" # insert query: <table_name> (<id>, <user_name>, <password>)
 cursor.execute(insert_query, user) # it can replace question marks smartly
 
-# use list of tuples to create bunch of users info
+# use list of tuples to insert bunch of users
 users = [
     (2, 'jose', 'asdf'),
-    (1, 'rolf', 'xyz')
+    (3, 'rolf', 'xyz')
 ]
 cursor.executemany(insert_query, users)
+
+# retrieve data
+select_query = "SELECT * FROM users"
+
+for row in cursor.execute(select_query):
+    print(row)
+
 
 
 connection.commit()
