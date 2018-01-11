7874b5072d463b262cf08e86a385cb1c032cbe19
Junrui Line <raymond.jr.lin@gmail.com>
2018-01-10 20:05:29 -0500

    change codes to store records in SQLite database instead of in in-memory database

--- store-resource-in-sql-db/code/security.py
+++ store-resource-in-sql-db/code/security.py
@@ -11,10 +11,12 @@ from user import User
 # ]
 ###
 
+### interact with database instead of hardcode
 # use User class to replace direct list
-users = [
-    User(1, 'bob', 'asdf')
-]
+# users = [
+#     User(1, 'bob', 'asdf')
+# ]
+###
 
 # use mapping so we don't need to iterate the list everytime
 
@@ -33,13 +35,19 @@ users = [
 #     }
 # }
 ###
-# simple the mapping: but still key-value pair in dictionary
-username_mapping = {u.username: u for u in users}
-userid_mapping = {u.id: u for u in users}
+
+### interact with database so no need mappings
+# simplify the mapping: but still key-value pair in dictionary
+# username_mapping = {u.username: u for u in users}
+# userid_mapping = {u.id: u for u in users}
+###
+
 
 def authenticate(username, password):
-    user = username_mapping.get(username, None)
-    # use a safer way - Safe String Compare - to compare string 
+    # user = username_mapping.get(username, None)
+    # change to interact with database
+    user = User.find_by_username(username)
+    # use a safer way - Safe String Compare - to compare string
     # if user and user.password == password:
     if user and safe_str_cmp(user.password, password):
         return user
@@ -47,4 +55,5 @@ def authenticate(username, password):
 def identity(payload):
     # what we got is JWT payload so we're going to extract the user ID from the payload
     user_id = payload['identity']
-    return userid_mapping.get(user_id, None)
+    # return userid_mapping.get(user_id, None)
+    return User.find_by_id(user_id)
