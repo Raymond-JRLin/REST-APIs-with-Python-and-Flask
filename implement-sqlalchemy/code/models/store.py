842611e3240d1f78fb2c5d5482cb41b1ca8841cc
Junrui Line <raymond.jr.lin@gmail.com>
2018-01-13 17:36:40 -0500

    modified json() and think a trade-off

--- implement-sqlalchemy/code/models/store.py
+++ implement-sqlalchemy/code/models/store.py
@@ -7,14 +7,15 @@ class StoreModel(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(80)) # 80 character maximum to limit the size
 
-    # back reference: look items to see which items are in this store
-    items = db.relationship('ItemModel')
+    # back reference: look items to see which items are in this store. we do this because prevent it looking into database to create items everytime when we create store model, and only create when we call json()
+    # but it will look in database everytime when we call json(), so it's a trade-off between creation and calling json()
+    items = db.relationship('ItemModel', lazy = 'dynamic') # when we do lazy = 'dynamic, the self.items in json(self) became a query builder
 
     def __init__(self, name):
         self.name = name
 
     def json(self):
-        return {'name': self.name, 'items': [item.json() for item in self.items]}
+        return {'name': self.name, 'items': [item.json() for item in self.items.all()]}
 
     # keep this as class method since it will return a dictionary other than a model object
     @classmethod
