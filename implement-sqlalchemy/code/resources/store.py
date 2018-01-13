c06044092ddcf9723ca4159b00e393df66318898
Junrui Line <raymond.jr.lin@gmail.com>
2018-01-13 17:54:30 -0500

    completed store and storeList methods, like get, post and delete

--- implement-sqlalchemy/code/resources/store.py
+++ implement-sqlalchemy/code/resources/store.py
@@ -3,13 +3,31 @@ from models.store import StoreModel
 
 class Store(Resource):
     def get(self, name):
-        pass
+        store = StoreModel.find_by_name(name)
+        if store:
+            return store.json()
+        return {'message': 'Store not found'}, 404
 
     def post(self, name):
-        pass
+        if StoreModel.find_by_name(name):
+            return {'message': "A store with name '{}' already exists.".format(name)}, 400
+
+        store = StoreModel(name)
+        try:
+            store.save_to_db()
+        except Exception as e:
+            return {'message': 'An error ocurred while creating the store.'}, 500
+
+        return store.json(), 201
 
     def delete(self, name):
-        pass
+        store = StoreModel.find_by_name(name)
+        if store:
+            store.delete_from_db
+        # if the store does not exist, we return this message directly, since users don't care whether it exists first but make sure it's no longer there
+        return {'message': 'Store deleted'}
 
 
 class StoreList(Resource):
+    def get(self):
+        return {'store': [store.json() for store in StoreModel.query.all()]}
